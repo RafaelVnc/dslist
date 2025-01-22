12eeb3c1f6fddfa6d71861601e2cfbcc7e71de22
Rafael Venancio <rafael.venancio08.06@gmail.com>
2025-01-22 17:36:26 -0300

    GET games by list

--- src/main/java/com/dslist/dslist/services/GameService.java
+++ src/main/java/com/dslist/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dslist.dslist.dto.GameDTO;
 import com.dslist.dslist.dto.GameMinDTO;
 import com.dslist.dslist.entities.Game;
+import com.dslist.dslist.projections.GameMinProjection;
 import com.dslist.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
